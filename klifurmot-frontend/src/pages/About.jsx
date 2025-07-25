84ddd822003928ddda2587d264d79f5dde1aa6fe
solonthorberg <solonhelgason@gmail.com>
2025-07-25 15:32:29 +0000

    need uptodate

--- klifurmot-frontend/src/pages/About.jsx
+++ klifurmot-frontend/src/pages/About.jsx
@@ -3,7 +3,9 @@ function About() {
   return (
     <>
       <Typography>
-        Klifurmót.is er í vinnslu. Netfang: solonhelgason@gmail.com
+        Klifurmót.is er í vinnslu. Ef þú lendir í villu eða vilt sjá eitthvað
+        betrumbætt endilega hafa samband!
+        <br /> Netfang: solonhelgason@gmail.com
       </Typography>
     </>
   );
