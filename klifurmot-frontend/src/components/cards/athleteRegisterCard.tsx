0855f8ba25dde2af00d9a6231244d7fc5924b214
solonthorberg <solonhelgason@gmail.com>
2026-05-27 20:54:16 +0000

    fix description length

--- klifurmot-frontend/src/components/cards/athleteRegisterCard.tsx
+++ klifurmot-frontend/src/components/cards/athleteRegisterCard.tsx
@@ -9,9 +9,10 @@ export default function AthleteRegisterCard({
 }) {
     return (
         <tr className="border-b border-outline last:border-0">
-            <td className="px-4 py-3">{athlete.full_name}</td>
-            <td className="px-4 py-3"></td>
-            <DisplayAthleteName Name={athlete.full_name} />
+            <td className="px-4 py-3">
+                <DisplayAthleteName Name={athlete.full_name} />
+            </td>
+            <td className="px-4 py-3">{athlete.category_name}</td>
             <td className="px-4 py-3 ">{athlete.gender}</td>
             <td className="px-4 py-3 text-right">
                 {getFlagEmoji(athlete.nationality)}
