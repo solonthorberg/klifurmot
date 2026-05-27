4e571138963949b4859bc60e2a5c35f595a16797
solonthorberg <solonhelgason@gmail.com>
2026-05-27 15:40:37 +0000

    fix safari viewport attempt 4

--- klifurmot-frontend/src/layouts/mainLayout.tsx
+++ klifurmot-frontend/src/layouts/mainLayout.tsx
@@ -6,7 +6,7 @@ export default function MainLayout() {
     return (
         <div className="flex flex-col h-full">
             <Navbar />
-            <main className="flex-1 min-h-0">
+            <main className="flex-1 min-h-0 overflow-auto">
                 <Outlet />
             </main>
         </div>
