ff9e64ca2db27e96b348ddee6d1207ec9d8968f0
solonthorberg <solonhelgason@gmail.com>
2026-06-03 16:25:12 +0000

    modified the live pill

--- klifurmot-frontend/src/components/ui/livePill.tsx
+++ klifurmot-frontend/src/components/ui/livePill.tsx
@@ -1,7 +1,5 @@
 export default function LivePill() {
     return (
-        <div className="border-2 border-red-500 bg-red-100 text-red-500 text-xs w-fit py-1 px-2 rounded-full">
-            Í gangi
-        </div>
+        <div className="border border-red-500 bg-red-500 p-2 rounded-full animate-pulse" />
     );
 }
